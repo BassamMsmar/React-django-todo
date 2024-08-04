4cca00029f2633fb7ec2e13f9e0d23b0b8764e9c
Bassam Msmar <48624392+BassamMsmar@users.noreply.github.com>
2024-08-04 22:39:55 +0300

    add list tasks

--- frontend/to do/src/App.jsx
+++ frontend/to do/src/App.jsx
@@ -7,11 +7,13 @@ import ToDo from "./components/ToDo";
 import CreateTodo from "./components/CreateToDo";
 
 function App() {
+
+  const todos =['react', 'bootstrap', 'vite']
   return (
       <div className="container">
         <div className="row">
           <div className="col-md-8 mx-auto my-5">
-            <ToDo />
+            <ToDo todos={todos}/>
             <CreateTodo />
           </div>
         </div>
--- frontend/to do/src/components/ToDo.jsx
+++ frontend/to do/src/components/ToDo.jsx
@@ -1,9 +1,23 @@
-const ToDo = () => {
+import { useState } from "react";
+function ToDo() {
+  const [name, setName] = useState("Bassam");
+  const updateName = () => {
+
+    if (name === "Ahmed") {
+      setName("Bassam");
+    } else {
+      setName("Ahmed");
+    }
+
+  };
+
   return (
     <div>
       <h3>ToDo</h3>
+      <h4>{name}</h4>
+      <button onClick={updateName}>Click me</button>
     </div>
   );
-};
+}
 
 export default ToDo;
