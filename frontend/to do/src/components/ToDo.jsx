99f7904c867da22931e443165b435f81a64689ce
Bassam Msmar <48624392+BassamMsmar@users.noreply.github.com>
2024-08-10 18:58:36 +0300

    add fetch to change staus

--- frontend/to do/src/components/ToDo.jsx
+++ frontend/to do/src/components/ToDo.jsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
 
 function ToDo({ todos, setTodos }) {
+  const handleStatus = (todo) => {
+    const updateTodo = todo.status === "Done" ? "InProgress" : "Done";
+    fetch(`http://127.0.0.1:8000/todo/${todo.id}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: todo.title, status: updateTodo }),
+    });
+  };
+
   return (
     <div className="container mt-4">
       <ul className="list-group">
@@ -10,7 +21,10 @@ function ToDo({ todos, setTodos }) {
             className="list-group-item d-flex justify-content-between"
           >
             <span>{todo.title}</span>
-            <button className="btn btn-sm border btn-primary">
+            <button
+              className="btn btn-sm border btn-primary"
+              onClick={() => handleStatus(todo)}
+            >
               {todo.status}
             </button>
           </li>
